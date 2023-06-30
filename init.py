6bc30ae2049cc5e56c548332a5863c5f2e30d8e7
Gabriel Campos <gcampos@hyl.cl>
2023-06-30 18:40:28 -0400

    funcionalidad ok

--- init.py
+++ init.py
@@ -1,8 +1,12 @@
 import main
 
-# Ejemplo de uso
-root_folder = r'c:\Users\DevHyL\Desktop\proyectos_python\test_input'
-exclusion_file = r'C:\Users\DevHyL\Desktop\proyectos_python\test_input\exclusiones.txt'
-output_zip = r'c:\Users\DevHyL\Desktop\proyectos_python\test_output\test_output.zip'
+# ruta del proyecto a comprimir
+root_folder = r'c:\Users\dev-h\OneDrive\Escritorio\proyectos_python\test_input'
+# ruta del archivo .txt que lista archivos a excluir
+exclusion_file = r'c:\Users\dev-h\OneDrive\Escritorio\proyectos_python\test_input\exclusiones.txt'
+# ruta destino zip resultante
+output_zip = r'c:\Users\dev-h\OneDrive\Escritorio\proyectos_python\test_output'
+# nombre zip
+zip_name = 'output.zip'
 
-main.zip_folder(root_folder, exclusion_file, output_zip)
+main.zip_folder(root_folder, exclusion_file, output_zip, zip_name)
--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import os
 import zipfile
 
-def zip_folder(root_folder, exclusion_file, output_zip):
-    # Lista de archivos y carpetas excluidos
-    exclusions = []
-    
-    # Leer el archivo de exclusiones
-    with open(exclusion_file, 'r') as file:
-        exclusions = file.read().splitlines()
-    
-    # Crear un archivo ZIP
-    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        # Recorrer todas las carpetas y archivos
-        for root, dirs, files in os.walk(root_folder):
-            # Eliminar las carpetas excluidas de la lista de directorios
-            dirs[:] = [d for d in dirs if d not in exclusions]
-            
-            for file in files:
-                if file not in exclusions:
-                    # Ruta completa del archivo
+def zip_folder(root_folder, exclusion_file, output_zip, zip_name):
+    try:
+        # Construyo ruta completa al zip
+        output_zip = output_zip + f"\{zip_name}"
+
+        # Lista de archivos y carpetas excluidos
+        exclusions = []
+        
+        # Leer el archivo de exclusiones
+        with open(exclusion_file, 'r') as file:
+            exclusions = file.read().splitlines()
+        
+        # Crear un archivo ZIP
+        with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            # Recorrer todas las carpetas y archivos
+            for root, dirs, files in os.walk(root_folder):
+                # Eliminar las carpetas excluidas de la lista de directorios
+                dirs[:] = [d for d in dirs if os.path.join(root, d) not in exclusions]
+                
+                for file in files:
                     file_path = os.path.join(root, file)
                     
                     # Ruta relativa dentro del archivo ZIP
                     zip_path = os.path.relpath(file_path, root_folder)
                     
-                    # Agregar el archivo al ZIP
-                    zipf.write(file_path, zip_path)
-
-
+                    # Verificar si el archivo completo (incluyendo la ruta) está excluido
+                    excluded = False
+                    for exclusion in exclusions:
+                        if (file_path.endswith(exclusion) or os.path.join(root, exclusion) == file_path) and exclusion.startswith("\\"):
+                            excluded = True
+                            break
+                    
+                    if not excluded:
+                        # Agregar el archivo al ZIP
+                        zipf.write(file_path, zip_path)
+    except Exception as e:
+        print(f'Error: {str(e)}')                        
